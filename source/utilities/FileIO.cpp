d367e4f3b18318d3502daf457f0f6bd465c4c752
Simone Raimondi <simo@cleverit.ch>
2019-03-01 12:56:38 +0100

    Added small utility function to read whole file content

---
+++ source/utilities/FileIO.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Simon on 2019-03-01.
+//
+
+#include "FileIO.hpp"
+
+#include <fstream>
+#include <sstream>
+
+namespace IO
+{
+    std::string ReadFile(const std::string& filename)
+    {
+        std::ifstream file(filename);
+        if (!file.is_open())
+        {
+            std::ostringstream error_message;
+            error_message << "Could not open file: " << filename;
+            throw std::invalid_argument(error_message.str());
+        }
+
+        // Read file content
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+
+        return buffer.str();
+    }
+
+} // IO namespace
---
+++ source/utilities/FileIO.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Simon on 2019-03-01.
+//
+
+#ifndef RABBIT_FILEIO_HPP
+#define RABBIT_FILEIO_HPP
+
+#include <string>
+
+namespace IO
+{
+    // Read file content into string
+    std::string ReadFile(const std::string& filename);
+
+} // IO namespace
+
+#endif //RABBIT_FILEIO_HPP
