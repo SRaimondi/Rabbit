9aad8f3d7bc9659668e25b3ec4b27aabba57ec85
Simone Raimondi <simo@cleverit.ch>
2019-03-01 14:43:32 +0100

    Added C++ OpenCL wrapper

--- source/rendering/TracingData.cpp
+++ source/rendering/TracingData.cpp
@@ -4,153 +4,48 @@
 
 #include "TracingData.hpp"
 
-Rays::Rays(cl_context context, unsigned int num_rays)
-    : num_rays{ num_rays }
-{
-    cl_int err_code{ CL_SUCCESS };
-    const size_t alloc_size_f = Size() * sizeof(cl_float);
+Rays::Rays(const cl::Context& context, unsigned int num_rays)
+    : num_rays{ num_rays },
+      origin_x{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      origin_y{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      origin_z{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      direction_x{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      direction_y{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      direction_z{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      extent{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_float) },
+      depth{ context, CL_MEM_READ_WRITE, num_rays * sizeof(cl_uint) }
+{}
 
-    // Allocate memory on the device
-    origin_x = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    origin_y = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    origin_z = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
+Intersections::Intersections(const cl::Context& context, unsigned int num_intersections)
+    : num_intersections(num_intersections),
+      hit_point_x{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      hit_point_y{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      hit_point_z{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      normal_x{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      normal_y{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      normal_z{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      uv_s{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      uv_t{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_float) },
+      primitive_index{ context, CL_MEM_READ_WRITE, num_intersections * sizeof(cl_uint) }
+{}
 
-    direction_x = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    direction_y = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    direction_z = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
+Samples::Samples(const cl::Context& context, unsigned int num_samples)
+    : num_samples{ num_samples },
+      Li_r{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      Li_g{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      Li_b{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      beta_r{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      beta_g{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      beta_b{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      pixel_x{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_uint) },
+      pixel_y{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_uint) },
+      sample_offset_x{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) },
+      sample_offset_y{ context, CL_MEM_READ_WRITE, num_samples * sizeof(cl_float) }
+{}
 
-    extent = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
+XOrShift::XOrShift(const cl::Context& context, unsigned int num_generators)
+    : num_generators{ num_generators },
+      state{ context, CL_MEM_READ_WRITE, num_generators * sizeof(cl_uint) }
+{}
 
-    depth = clCreateBuffer(context, CL_MEM_READ_WRITE, Size() * sizeof(cl_uint), nullptr, &err_code);
-    CHECK_CL(err_code);
-}
-
-Rays::~Rays() noexcept
-{
-    CHECK_LOG_CL(clReleaseMemObject(origin_x));
-    CHECK_LOG_CL(clReleaseMemObject(origin_y));
-    CHECK_LOG_CL(clReleaseMemObject(origin_z));
-
-    CHECK_LOG_CL(clReleaseMemObject(direction_x));
-    CHECK_LOG_CL(clReleaseMemObject(direction_y));
-    CHECK_LOG_CL(clReleaseMemObject(direction_z));
-
-    CHECK_LOG_CL(clReleaseMemObject(extent));
-    CHECK_LOG_CL(clReleaseMemObject(depth));
-}
-
-Intersections::Intersections(cl_context context, unsigned int num_intersections)
-    : num_intersections(num_intersections)
-{
-    cl_int err_code{ CL_SUCCESS };
-    const size_t alloc_size_f = Size() * sizeof(cl_float);
-
-    // Allocate memory on the device
-    hit_point_x = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    hit_point_y = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    hit_point_z = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-
-    normal_x = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    normal_y = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    normal_z = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-
-    uv_s = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    uv_t = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-
-    primitive_index = clCreateBuffer(context, CL_MEM_READ_WRITE, Size() * sizeof(cl_uint), nullptr, &err_code);
-    CHECK_CL(err_code);
-}
-
-Intersections::~Intersections() noexcept
-{
-    CHECK_LOG_CL(clReleaseMemObject(hit_point_x));
-    CHECK_LOG_CL(clReleaseMemObject(hit_point_y));
-    CHECK_LOG_CL(clReleaseMemObject(hit_point_z));
-
-    CHECK_LOG_CL(clReleaseMemObject(normal_x));
-    CHECK_LOG_CL(clReleaseMemObject(normal_y));
-    CHECK_LOG_CL(clReleaseMemObject(normal_z));
-
-    CHECK_LOG_CL(clReleaseMemObject(uv_s));
-    CHECK_LOG_CL(clReleaseMemObject(uv_t));
-
-    CHECK_LOG_CL(clReleaseMemObject(primitive_index));
-}
-
-Samples::Samples(cl_context context, unsigned int num_samples)
-    : num_samples{ num_samples }
-{
-    cl_int err_code{ CL_SUCCESS };
-    const size_t alloc_size_f = Size() * sizeof(cl_float);
-
-    // Allocate memory on the device
-    Li_r = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    Li_g = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    Li_b = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-
-    beta_r = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    beta_g = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    beta_b = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-
-    pixel_x = clCreateBuffer(context, CL_MEM_READ_WRITE, Size() * sizeof(cl_uint), nullptr, &err_code);
-    CHECK_CL(err_code);
-    pixel_y = clCreateBuffer(context, CL_MEM_READ_WRITE, Size() * sizeof(cl_uint), nullptr, &err_code);
-    CHECK_CL(err_code);
-
-    sample_offset_x = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-    sample_offset_y = clCreateBuffer(context, CL_MEM_READ_WRITE, alloc_size_f, nullptr, &err_code);
-    CHECK_CL(err_code);
-}
-
-Samples::~Samples() noexcept
-{
-    CHECK_LOG_CL(clReleaseMemObject(Li_r));
-    CHECK_LOG_CL(clReleaseMemObject(Li_g));
-    CHECK_LOG_CL(clReleaseMemObject(Li_b));
-
-    CHECK_LOG_CL(clReleaseMemObject(beta_r));
-    CHECK_LOG_CL(clReleaseMemObject(beta_g));
-    CHECK_LOG_CL(clReleaseMemObject(beta_b));
-
-    CHECK_LOG_CL(clReleaseMemObject(pixel_x));
-    CHECK_LOG_CL(clReleaseMemObject(pixel_y));
-
-    CHECK_LOG_CL(clReleaseMemObject(sample_offset_x));
-    CHECK_LOG_CL(clReleaseMemObject(sample_offset_y));
-}
-
-XOrShift::XOrShift(cl_context context, unsigned int num_generators)
-    : num_generators{ num_generators }
-{
-    cl_int err_code = CL_SUCCESS;
-    state = clCreateBuffer(context, CL_MEM_READ_WRITE, Size() * sizeof(cl_uint), nullptr, &err_code);
-    CHECK_CL(err_code);
-}
-
-XOrShift::~XOrShift() noexcept
-{
-    CHECK_LOG_CL(clReleaseMemObject(state));
-}
 
--- source/rendering/TracingData.hpp
+++ source/rendering/TracingData.hpp
@@ -5,124 +5,95 @@
 #ifndef RABBIT_TRACINGDATA_HPP
 #define RABBIT_TRACINGDATA_HPP
 
-#include "CLError.hpp"
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#include "cl2.hpp"
 
 // Storage class for the Rays data
 struct Rays
 {
-    Rays(cl_context context, unsigned int num_rays);
+    Rays(const cl::Context& context, unsigned int num_rays);
 
-    ~Rays() noexcept;
+    const unsigned int num_rays;
 
     // Origin
-    cl_mem origin_x;
-    cl_mem origin_y;
-    cl_mem origin_z;
+    cl::Buffer origin_x;
+    cl::Buffer origin_y;
+    cl::Buffer origin_z;
 
     // Direction
-    cl_mem direction_x;
-    cl_mem direction_y;
-    cl_mem direction_z;
+    cl::Buffer direction_x;
+    cl::Buffer direction_y;
+    cl::Buffer direction_z;
 
     // Ray extent
-    cl_mem extent;
+    cl::Buffer extent;
 
     // Current depth of the ray (cl_uint)
-    cl_mem depth;
-
-    unsigned int Size() const noexcept
-    {
-        return num_rays;
-    }
-
-private:
-    const unsigned int num_rays;
+    cl::Buffer depth;
 };
 
 // Storage class for the intersection information
 struct Intersections
 {
-    explicit Intersections(cl_context context, unsigned int num_intersections);
+    Intersections(const cl::Context& context, unsigned int num_intersections);
 
-    ~Intersections() noexcept;
+    const unsigned int num_intersections;
 
     // Hit point
-    cl_mem hit_point_x;
-    cl_mem hit_point_y;
-    cl_mem hit_point_z;
+    cl::Buffer hit_point_x;
+    cl::Buffer hit_point_y;
+    cl::Buffer hit_point_z;
 
     // Normal
-    cl_mem normal_x;
-    cl_mem normal_y;
-    cl_mem normal_z;
+    cl::Buffer normal_x;
+    cl::Buffer normal_y;
+    cl::Buffer normal_z;
 
     // UV coordinates
-    cl_mem uv_s;
-    cl_mem uv_t;
+    cl::Buffer uv_s;
+    cl::Buffer uv_t;
 
-    // Index of the intersected primitive
-    cl_mem primitive_index;
-
-    unsigned int Size() const noexcept
-    {
-        return num_intersections;
-    }
-
-private:
-    const unsigned int num_intersections;
+    // Index of the intersected primitive (cl_uint)
+    cl::Buffer primitive_index;
 };
 
 // Samples taken in the image plane
 struct Samples
 {
-    Samples(cl_context context, unsigned int num_samples);
+    Samples(const cl::Context& context, unsigned int num_samples);
 
-    ~Samples() noexcept;
+    const unsigned int num_samples;
 
     // Incoming radiance
-    cl_mem Li_r;
-    cl_mem Li_g;
-    cl_mem Li_b;
+    cl::Buffer Li_r;
+    cl::Buffer Li_g;
+    cl::Buffer Li_b;
 
     // Accumulation mask for current path
-    cl_mem beta_r;
-    cl_mem beta_g;
-    cl_mem beta_b;
+    cl::Buffer beta_r;
+    cl::Buffer beta_g;
+    cl::Buffer beta_b;
 
     // Sample pixel coordinates
-    cl_mem pixel_x;
-    cl_mem pixel_y;
+    cl::Buffer pixel_x;
+    cl::Buffer pixel_y;
 
     // Sample offset in the pixel
-    cl_mem sample_offset_x;
-    cl_mem sample_offset_y;
-
-    unsigned int Size() const noexcept
-    {
-        return num_samples;
-    }
-
-private:
-    const unsigned int num_samples;
+    cl::Buffer sample_offset_x;
+    cl::Buffer sample_offset_y;
 };
 
 // XOrShift status, it's a very simple generator but good enough for testing and also has 32bit status
 struct XOrShift
 {
-    XOrShift(cl_context context, unsigned int num_generators);
+    XOrShift(const cl::Context& context, unsigned int num_generators);
 
-    ~XOrShift() noexcept;
+    const unsigned int num_generators;
 
     // Status of the generator
-    cl_mem state;
-
-    unsigned int Size() const noexcept
-    {
-        return num_generators;
-    }
-
-private:
-    const unsigned int num_generators;
+    cl::Buffer state;
 };
 
 #endif //RABBIT_TRACINGDATA_HPP
